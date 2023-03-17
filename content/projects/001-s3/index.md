5fe38e05fcc8e1b970956271d24bf5d201232039
HelleGazelle <alois.niedermaier@googlemail.com>
2023-03-17 16:05:16 +0100

    adding custom colors

---
+++ content/projects/001-s3/index.md
@@ -0,0 +1,6 @@
+---
+title: S3 sample project
+draft: false
+summary: Jarrett is a code enthusiast from the Central Michigan area.
+type: projects
+---
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ['layouts/**/*.html'],
   media: false, // or 'media' or 'class'
   theme: {
-    
+    colors: {
+      white: colors.white,
+      purple: {
+        500: "#865DFF",
+        900: "#531df4"
+      },
+      gray: colors.gray,
+      violet: {
+        500: "#E384FF"
+      },
+      transparent: colors.transparent,
+      black: "#191825",
+      pink: {
+        400: "#fcb3fa",
+        500: "#FFA3FD",
+        600: "#f78af3",
+        800: "#ef62ea"
+      }
+    }
   },
   variants: {
     extend: {},
